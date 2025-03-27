d03595baeafaecb251001cac1f759f87da35ee51
chahyeon min <chahyeonmin91@gmail.com>
2025-03-27 13:35:42 +0900

    level3

---
+++ src/level3/Q11021.java
@@ -0,0 +1,22 @@
+package level3;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Q11021 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            bw.write("Case #"+(i+1)+": "+ (Integer.parseInt(st.nextToken())+ Integer.parseInt(st.nextToken()))+"\n");
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+
+    }
+}
